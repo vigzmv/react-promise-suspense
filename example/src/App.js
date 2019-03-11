388205571c2101f0451fc31079969c6e391f4a04
vigzmv <vigzmv@outlook.com>
2019-03-11 16:55:18 +0530

    feat(example): add

---
+++ example/src/App.js
@@ -0,0 +1,25 @@
+import React, { Suspense } from 'react';
+import usePromise from '../../';
+
+const fetchJson = input => fetch(input).then(res => console.log(res) || res.json());
+
+const MyFetchingComponent = () => {
+  const data = usePromise(fetchJson, [
+    'https://pokeapi.co/api/v2/pokemon/ditto/',
+    { method: 'GET' },
+  ]);
+
+
+  console.log(data);
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+};
+
+const App = () => {
+  return (
+    <Suspense fallback="Loading...">
+      <MyFetchingComponent />
+    </Suspense>
+  );
+};
+
+export default App;
